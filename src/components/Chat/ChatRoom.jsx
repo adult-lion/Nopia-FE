970d63036dd5f5cf033701f7511390149d291998
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2023-08-15 23:17:39 +0900

    [fix] 재렌더링 시 시간 초기화 오류 해결

--- src/components/Chat/ChatRoom.jsx
+++ src/components/Chat/ChatRoom.jsx
@@ -18,8 +18,15 @@ import image3 from "../../assets/images/image3.jpg";
 import image4 from "../../assets/images/image4.jpg";
 import image5 from "../../assets/images/image5.jpg";
 import image6 from "../../assets/images/image6.jpg";
+import { useState, useEffect } from "react";
 
 const ChatRoom = () => {
+  // 카운트 다운 시간을 브라우저의 로컬 스토리지에 저장하여 새로고침 후에도 값을 유지할 수 있도록 한다.
+  const [countdownEndTime, setCountdownEndTime] = useState(
+    parseInt(localStorage.getItem("countdownEndTime")) ||
+      Date.now() + 1000 * 60 * 4.99
+  );
+
   const people = [
     { name: "익명1", img: image1 },
     { name: "익명2", img: image2 },
@@ -46,12 +53,19 @@ const ChatRoom = () => {
       );
     }
   };
+
+  // 컴포넌트가 렌더링 될 때(새로고침 때)마다, 그리고 countdownEndTime이 변경될 때마다 로컬 스토리지 countdownEndTime 값을 변경한다(문자열로).
+  useEffect(() => {
+    // Save the countdown end time to local storage
+    localStorage.setItem("countdownEndTime", countdownEndTime.toString());
+  }, [countdownEndTime]);
+
   return (
     <>
       <div
         style={{ width: "100%", backgroundColor: "white", padding: "15px 0" }}
       >
-        <Countdown date={Date.now() + 1000 * 60 * 4.99} renderer={renderer} />
+        <Countdown date={countdownEndTime} renderer={renderer} />
       </div>
       <div
         style={{
